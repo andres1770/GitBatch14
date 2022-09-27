fc406c34e3a88b534e38df0fd81282853e0fe856
andres1770 <andresmoron1770@gmail.com>
2022-09-27 19:50:17 -0400

    second commit

--- src/batch14/Class03B14.java
+++ src/batch14/Class03B14.java
@@ -5,6 +5,7 @@ public class Class03B14 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hey this is a git class");
+System.out.println("hey this is a change in my project");
 	}
 
 }
